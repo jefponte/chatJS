e4f5bc57f012cedc16c4e527d5c2ce64603c409c
Jefferson Uchôa Ponte <jefponte@unilab.edu.br>
2021-08-13 19:19:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var io = require('socket.io')(3000);
 
 io.on('connection', (socket) => {
-    console.log("novo usuario conectado");
+    console.log("New User Connected");
     socket.on("client_mensage", (data)=>{
         io.sockets.emit("server_mensage", data);
     });
